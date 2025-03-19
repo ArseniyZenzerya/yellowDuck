fb0d1d5c5761adb0a2ed4967280f1bfc1bf064eb
Arseniy <72448125+SenyaZ@users.noreply.github.com>
2025-03-19 17:31:51 +0200

    Add trello service to service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Console\Commands\CreateTrelloBoard;
 use App\Contracts\UserServiceInterface;
 use App\Services\TelegramService;
 use App\Services\TelegramWebhookService;
+use App\Services\TrelloService;
 use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,8 +21,9 @@ class AppServiceProvider extends ServiceProvider
 
         $this->app->singleton(TelegramWebhookService::class, function ($app) {
             $telegramService = $app->make(TelegramService::class);
+            $trelloService = $app->make(TrelloService::class);
             $userService = $app->make(UserServiceInterface::class);
-            return new TelegramWebhookService($telegramService, $userService);
+            return new TelegramWebhookService($telegramService, $userService, $trelloService);
         });
 
         $this->app->singleton(UserService::class, function ($app) {
